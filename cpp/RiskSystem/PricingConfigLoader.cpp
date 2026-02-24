7e71c111ce0af5060330afe42a89791a6c08baf1
croudxd <benjamincroud@gmail.com>
2026-02-24 20:45:30 +0000

    Third test passing all problems, this was slightly easier to me than the above problem, simple wrote a simple function that will parse the xml, Create a PricingEngineConfigItem with the data, and then we push that back onto the PricingEngineConfig vector.

--- cpp/RiskSystem/PricingConfigLoader.cpp
+++ cpp/RiskSystem/PricingConfigLoader.cpp
@@ -1,5 +1,8 @@
 #include "PricingConfigLoader.h"
 #include <stdexcept>
+#include <fstream>
+#include "../Models/BondTrade.h"
+#include "../Models/FxTrade.h"
 
 std::string PricingConfigLoader::getConfigFile() const {
     return configFile_;
@@ -9,6 +12,73 @@ void PricingConfigLoader::setConfigFile(const std::string& file) {
     configFile_ = file;
 }
 
-PricingEngineConfig PricingConfigLoader::loadConfig() {
-    throw std::runtime_error("Not implemented");
+std::string extractAttribute(const std::string& line, const std::string& attributeName) {
+    std::string searchStr = attributeName + "=\"";
+    size_t startPos = line.find(searchStr);
+    
+    if (startPos == std::string::npos) {
+        return "";
+    }
+    
+    startPos += searchStr.length();
+    
+    size_t endPos = line.find("\"", startPos);
+    
+    if (endPos == std::string::npos) {
+        return ""; 
+    }
+    
+    return line.substr(startPos, endPos - startPos);
 }
+
+PricingEngineConfig PricingConfigLoader::loadConfig() {
+    std::string filename = getConfigFile();
+    std::ifstream stream(filename);
+    
+    if (!stream.is_open()) {
+        throw std::runtime_error("Cannot open config file: " + filename);
+    }
+    
+    PricingEngineConfig config; 
+    std::string line;
+    
+    while (std::getline(stream, line)) {
+        if (line.find("<Engine ") == std::string::npos) {
+            continue;
+        }
+        
+        std::string tradeType = extractAttribute(line, "tradeType");
+        std::string assembly = extractAttribute(line, "assembly");
+        std::string pricingEngine = extractAttribute(line, "pricingEngine");
+        
+
+        PricingEngineConfigItem item;
+        item.setTypeName(pricingEngine);
+        item.setAssembly(assembly);
+
+        if (tradeType[0] == 'G')
+        {
+            item.setTradeType(BondTrade::GovBondTradeType);
+        }
+        if (tradeType[0] == 'C')
+        {
+            item.setTradeType(BondTrade::CorpBondTradeType);
+        }
+        if (tradeType[0] == 'F')
+        {
+            if (tradeType[2] == 'S')
+            {
+                item.setTradeType(FxTrade::FxSpotTradeType);
+            }
+            else
+            {
+                item.setTradeType(FxTrade::FxForwardTradeType);
+            }
+        }
+
+        config.push_back(item);
+    }
+    
+    return config;
+}
+
